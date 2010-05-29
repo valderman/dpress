58a6726637e74dafc21f4eb6ea61ad32c856cf15
Anton Ekblad <anton@ekblad.cc>
2010-05-29 02:53:07 +0200

    Export functions to get max and preferred key lenghts.

--- DissociatedPress.hs
+++ DissociatedPress.hs
@@ -1,12 +1,12 @@
 {-# LANGUAGE OverloadedStrings #-}
 module DissociatedPress (
     Dictionary, Word,
-    newDict, defDict, updateDict, setPreferredKeyLength, toKey, optKey,
+    maxKeyLen, preferKeyLen, setPreferredKeyLength,
+    newDict, defDict, updateDict, toKey, optKey,
     disPress, disPressBack, -- randomPress,
     randomSentence, ask, insertText,
     store, load
   ) where
-import Data.ByteString.Lazy.Char8
 import DissociatedPress.Core
 import DissociatedPress.Storage
 import DissociatedPress.Text.ByteString
