edf56eafb0414efc1ec3aba25eb454c1d7d19c34
Anton Ekblad <anton@ekblad.cc>
2010-05-30 14:02:08 +0200

    Added simple conversation test program.

---
+++ test.hs
@@ -0,0 +1,28 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Main where
+import DissociatedPress
+import System.Random
+import System.IO
+import System.Environment (getArgs)
+import qualified Data.ByteString.Lazy.Char8 as B
+
+main = do
+  a <- getArgs
+  if null a then error "Give the name of the dictionary on the command line!"
+            else return ()
+  d <- load (head a) >>= return . (\(Just d) -> d)
+  g <- newStdGen
+  putStrLn $  "Conversation with dictionary " ++ (head a) ++ "\n"
+           ++ "Max key length is " ++ show (maxKeyLen d) ++ " and "
+           ++ "preferred key length is " ++ show (preferKeyLen d)
+  B.putStr "> "
+  ss <- B.getContents
+  askSession d g (B.lines ss)
+ where
+  askSession d g (s:ss) = do
+    B.putStrLn $ ask s d g
+    hFlush stdout
+    B.putStr "> "
+    hFlush stdout
+    askSession d (snd (random g :: (Bool, StdGen))) ss
+  askSession _ _ _ = B.putStrLn ""
